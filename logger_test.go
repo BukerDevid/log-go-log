ce864e51c3f3b906bc1d08ffc7cb6679d83856b8
Guselnikov Vasiliy <mr.wgw@yandex.ru>
2021-11-17 16:14:49 +0200

    Проверка тестами

--- logger_test.go
+++ logger_test.go
@@ -4,19 +4,19 @@ import (
 	"os"
 	"testing"
 
-	. "github.com/bukerdevid/log-go-log"
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
 )
 
 var PKG_APP_LOGGER_WR_DFILE_PATH = "salair-back.re.log"
 
 func TestWriteLog(t *testing.T) {
-	InitCastomLogger(&JSONFormatter{TimestampFormat: "15:04:05 02/01/2006"}, TraceLevel, true, true)
+	InitCastomLogger(&logrus.JSONFormatter{TimestampFormat: "15:04:05 02/01/2006"}, logrus.TraceLevel, true, true)
 	defer func() {
 		err := recover()
 		if err != nil {
-			entry := err.(*Entry)
-			WithFields(Fields{
+			entry := err.(*logrus.Entry)
+			logrus.WithFields(logrus.Fields{
 				"omg":         true,
 				"err_size":    entry.Data["size"],
 				"err_level":   entry.Level,
@@ -27,43 +27,43 @@ func TestWriteLog(t *testing.T) {
 	}()
 
 	for count := 0; count < 10; count++ {
-		Info("Random data - ", count)
-		Debug("Random data - ", count)
-		Warn("Random data - ", count)
-		Error("Random data - ", count)
+		logrus.Info("Random data - ", count)
+		logrus.Debug("Random data - ", count)
+		logrus.Warn("Random data - ", count)
+		logrus.Error("Random data - ", count)
 	}
-	Panic("Check pnic")
+	logrus.Panic("Check pnic")
 }
 
 func TestAddOut(t *testing.T) {
-	logger.InitCastomLogger(&logger.JSONFormatter{TimestampFormat: "15:04:05 02/01/2006"}, logger.TraceLevel, true, true)
+	InitCastomLogger(&logrus.JSONFormatter{TimestampFormat: "15:04:05 02/01/2006"}, logrus.TraceLevel, true, true)
 	defer func() {
 		err := recover()
 		if err != nil {
-			entry := err.(*logger.Entry)
-			logger.WithFields(logger.Fields{
+			entry := err.(*logrus.Entry)
+			logrus.WithFields(logrus.Fields{
 				"omg":         true,
 				"err_size":    entry.Data["size"],
 				"err_level":   entry.Level,
 				"err_message": entry.Message,
 			}).Error("The ice breaks!")
 		}
-		logger.require.True(t, true)
+		require.True(t, true)
 	}()
 
 	for count := 0; count < 10; count++ {
-		logger.Info("Random data - ", count)
-		logger.Debug("Random data - ", count)
-		logger.Warn("Random data - ", count)
-		logger.Error("Random data - ", count)
+		logrus.Info("Random data - ", count)
+		logrus.Debug("Random data - ", count)
+		logrus.Warn("Random data - ", count)
+		logrus.Error("Random data - ", count)
 		if count == 5 {
 			file, err := os.OpenFile(PKG_APP_LOGGER_WR_DFILE_PATH, os.O_CREATE|os.O_WRONLY, 0666)
 			if err != nil {
-				logger.Fatal(err.Error())
-				logger.require.True(t, true)
+				logrus.Fatal(err.Error())
+				require.True(t, true)
 			}
-			logger.AddOut(file)
+			AddOut(file)
 		}
 	}
-	logger.Panic("Check pnic")
+	logrus.Panic("Check pnic")
 }
